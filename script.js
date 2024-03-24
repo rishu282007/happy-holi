60281cd4dc36a044b03ff50bc7c3964308ff3f39
Rishu <149008762+rishu282007@users.noreply.github.com>
2024-03-24 10:57:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let usr = prompt("What's Your Name Buddy🧐..")
+let name = document.getElementById("name").innerHTML = usr
+if (usr == "Ritesh" || usr =="Gulshan") {
+    let name = document.getElementById("name").innerHTML = usr + " Sir Ji"
+} 
+else if (usr =="ritesh" || usr == "gulshan") {
+    let name = document.getElementById("name").innerHTML = usr + " Sir Ji"
+}
+else if (usr =="Bhagwan" || usr == "bhagwan") {
+    let name = document.getElementById("name").innerHTML = usr + " Sir Ji"
+}
+else if (usr =="Bhagwan Kumar" || usr == "bhagwan kumar") {
+    let name = document.getElementById("name").innerHTML = usr + " Sir Ji"
+}
+else if (usr =="Bhagwan kumar" || usr == "bhagwan kumar") {
+    let name = document.getElementById("name").innerHTML = usr + " Sir Ji"
+}
